4a949edb9683f39600af3e63209a18e7d87d0b86
kennysexton <kennysexton4@gmail.com>
2020-12-11 17:48:12 -0500

    first commit

---
+++ js/bracketManager.js
@@ -0,0 +1,12 @@
+let team = new Object()
+team.color = "#0085CA"
+team.secondaryColor = "#101820"
+team.location = "Carolina"
+team.name = "Panthers"
+team.logo = "panthers.jpg" // Get this systematically, not stored
+
+
+var firstSeed = document.getElementById("A1");
+
+// Change Box to match teams color
+firstSeed.style.background = team.color;
