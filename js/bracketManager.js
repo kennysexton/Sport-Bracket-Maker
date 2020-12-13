fca51850695d7af7d65da76a4fcdf3ce7b7a09e2
kennysexton <kennysexton4@gmail.com>
2020-12-13 11:46:55 -0500

    adding more flexable JS for showing team specific info

--- js/bracketManager.js
+++ js/bracketManager.js
@@ -1,12 +1,30 @@
-let team = new Object()
-team.color = "#0085CA"
-team.secondaryColor = "#101820"
-team.location = "Carolina"
-team.name = "Panthers"
-team.logo = "panthers.jpg" // Get this systematically, not stored
+document.addEventListener("DOMContentLoaded", function(){
+	let team = new Object()
+	team.color = "#0085CA"
+	team.secondaryColor = "#101820"
+	team.location = "Carolina"
+	team.name = "Panthers"
+	team.logo = "panthers.jpg" // Get this systematically, not stored
 
 
-var firstSeed = document.getElementById("A1");
+	var firstSeed = document.getElementById("A1");
+	//	var firstSeedLine = document.getElementById("line")
 
-// Change Box to match teams color
-firstSeed.style.background = team.color;
+	console.log("-- Length of firstSeed: " + firstSeed)
+
+
+	// Change Box to match teams information
+	firstSeed.style.background = team.color;
+	//	firstSeedLine.style.background = team.secondaryColor;
+	firstSeed.innerText = team.location + " " +  team.name;
+
+
+	// Add the line element
+	const firstSeedLine = document.createElement("div"); 
+	firstSeedLine.setAttribute("id", "line")
+	firstSeed.appendChild(firstSeedLine); 
+
+	firstSeedLine.style.background = team.secondaryColor;
+	
+	//	console.log("-- Length of firstSeedLine: " + firstSeedLine)
+});
