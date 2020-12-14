8cf7faacafae830194d975a69fc54f545913c460
kennysexton <kennysexton4@gmail.com>
2020-12-13 19:44:57 -0500

    adding nfc round 1

--- js/bracketManager.js
+++ js/bracketManager.js
@@ -6,19 +6,21 @@ document.addEventListener("DOMContentLoaded", function(){
   var afcRound1Array = document.getElementsByClassName("AFC");
   var nfcRound1Array = document.getElementsByClassName("NFC");
 
-  console.log("teams - " + teams)
-  console.log("results - " + result)
+  console.log("teams - " + afcRound1Array.length)
+  console.log("results - " + nfcRound1Array .length)
 
+  // AFC Round 1
   for(var i=0; i< afcRound1Array.length; i++){
     // Get the right team index
     teamObject = binarySearch(teams, result.afcRound1[i])
-    firstRoundLogic(teamObject , i)
+    firstRoundLogic(teamObject, afcRound1Array)
   }
 
+  // NFC Round 1
   for(var i=0; i< nfcRound1Array.length; i++){
     // Get the right team index
-    teamIndex = binarySearch(teams, result.afcRound1[i])
-    firstRoundLogic(teamObject , i)
+    teamObject = binarySearch(teams, result.nfcRound1[i])
+    firstRoundLogic(teamObject , nfcRound1Array)
   }
 
   // Grab json files
@@ -35,8 +37,6 @@ document.addEventListener("DOMContentLoaded", function(){
   // credit - wOxxOm (https://codereview.stackexchange.com/questions/144821/binary-search-on-string-in-alphabetical-order)
   function binarySearch(haystack, needle) {
 
-    console.log("haystack - " + haystack)
-    console.log("Needle - " + needle)
     var a = 0;
     var b = haystack.length - 1;
     if (needle < haystack[0].city || needle > haystack[b].city) {
@@ -52,28 +52,28 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     //TODO check for this edge case.  multiple teams play in the same city edge case
     if(haystack[a].city == 'Los Angeles' || haystack[a].city == 'New York'){
-
+      return haystack[a]
     } else if (haystack[a].city === needle){
       return haystack[a]
     } else if (haystack[a+1] === needle) {
       return haystack[a+1]
     } else {
-      console.error("Something went wrong in binary search")
+      console.error("Something went wrong in binary search when searching for - " + needle)
       return {}
     }
   }
 
   // Logic used for getting populating first round teams
-  function firstRoundLogic(teamObject , i){
+  function firstRoundLogic(teamObject, divisionArray){
 
     // Change Box to match teams information
-    afcRound1Array[i].style.background = teamObject.cPrim;
-    afcRound1Array[i].innerText = teamObject.city + " " +  teamObject.name;
+    divisionArray[i].style.background = teamObject.cPrim;
+    divisionArray[i].innerText = teamObject.city + " " +  teamObject.name;
 
     // Line below name with secondary color
     const firstSeedLine = document.createElement("div"); 
     firstSeedLine.setAttribute("id", "line")
-    afcRound1Array[i].appendChild(firstSeedLine); 
+    divisionArray[i].appendChild(firstSeedLine); 
 
     firstSeedLine.style.background = teamObject.cSecn;
   }
