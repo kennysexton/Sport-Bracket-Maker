b90c0236193f271a6819aca383ab2173df9d68a4
kennysexton <kennysexton4@gmail.com>
2020-12-13 19:08:54 -0500

    first round progress for AFC division

--- js/bracketManager.js
+++ js/bracketManager.js
@@ -1,30 +1,81 @@
 document.addEventListener("DOMContentLoaded", function(){
-	let team = new Object()
-	team.color = "#0085CA"
-	team.secondaryColor = "#101820"
-	team.location = "Carolina"
-	team.name = "Panthers"
-	team.logo = "panthers.jpg" // Get this systematically, not stored
 
+  // Grab team JSON data
+  var teams = parseJson(data)
+  var result = parseJson(results)
+  var afcRound1Array = document.getElementsByClassName("AFC");
+  var nfcRound1Array = document.getElementsByClassName("NFC");
 
-	var firstSeed = document.getElementById("A1");
-	//	var firstSeedLine = document.getElementById("line")
+  console.log("teams - " + teams)
+  console.log("results - " + result)
 
-	console.log("-- Length of firstSeed: " + firstSeed)
+  for(var i=0; i< afcRound1Array.length; i++){
+    // Get the right team index
+    teamObject = binarySearch(teams, result.afcRound1[i])
+    firstRoundLogic(teamObject , i)
+  }
 
+  for(var i=0; i< nfcRound1Array.length; i++){
+    // Get the right team index
+    teamIndex = binarySearch(teams, result.afcRound1[i])
+    firstRoundLogic(teamObject , i)
+  }
 
-	// Change Box to match teams information
-	firstSeed.style.background = team.color;
-	//	firstSeedLine.style.background = team.secondaryColor;
-	firstSeed.innerText = team.location + " " +  team.name;
+  // Grab json files
+  function parseJson(jsonObject){
+    try {
+      return jsonObject
+    } catch (e) {
+      console.error("Failed to parse JSON: " + e )
+      return null
+    }
+  }
 
+  // grab the the team info that matches the results value. 
+  // credit - wOxxOm (https://codereview.stackexchange.com/questions/144821/binary-search-on-string-in-alphabetical-order)
+  function binarySearch(haystack, needle) {
 
-	// Add the line element
-	const firstSeedLine = document.createElement("div"); 
-	firstSeedLine.setAttribute("id", "line")
-	firstSeed.appendChild(firstSeedLine); 
+    console.log("haystack - " + haystack)
+    console.log("Needle - " + needle)
+    var a = 0;
+    var b = haystack.length - 1;
+    if (needle < haystack[0].city || needle > haystack[b].city) {
+      return {};
+    }
+    while (a < b - 1) {
+      var c = (a + b) / 2 |0;
+      if (needle < haystack[c].city) {
+        b = c;
+      } else {
+        a = c;
+      }
+    }
+    //TODO check for this edge case.  multiple teams play in the same city edge case
+    if(haystack[a].city == 'Los Angeles' || haystack[a].city == 'New York'){
 
-	firstSeedLine.style.background = team.secondaryColor;
-	
-	//	console.log("-- Length of firstSeedLine: " + firstSeedLine)
+    } else if (haystack[a].city === needle){
+      return haystack[a]
+    } else if (haystack[a+1] === needle) {
+      return haystack[a+1]
+    } else {
+      console.error("Something went wrong in binary search")
+      return {}
+    }
+  }
+
+  // Logic used for getting populating first round teams
+  function firstRoundLogic(teamObject , i){
+
+    // Change Box to match teams information
+    afcRound1Array[i].style.background = teamObject.cPrim;
+    afcRound1Array[i].innerText = teamObject.city + " " +  teamObject.name;
+
+    // Line below name with secondary color
+    const firstSeedLine = document.createElement("div"); 
+    firstSeedLine.setAttribute("id", "line")
+    afcRound1Array[i].appendChild(firstSeedLine); 
+
+    firstSeedLine.style.background = teamObject.cSecn;
+  }
+
 });
