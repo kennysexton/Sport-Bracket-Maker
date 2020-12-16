f19ae63f539b0770c310c395e8eb175adceb18d6
kennysexton <kennysexton4@gmail.com>
2020-12-16 08:57:34 -0500

    clean up some code.   AFC round 2 dropdown population

--- js/bracketManager.js
+++ js/bracketManager.js
@@ -1,132 +1,103 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  // Grab team JSON data
-  var teams = parseJson(data)
-  var result = parseJson(results)
-  var afcRound1Array = document.getElementsByClassName("AFC");
-  var nfcRound1Array = document.getElementsByClassName("NFC");
+	// Grab team JSON data
+	var teams = parseJson(data)
+	var result = parseJson(results)
+	var afcRound1Array = document.getElementsByClassName("AFC");
+	var nfcRound1Array = document.getElementsByClassName("NFC");
 
-  var afcRound2Array = document.getElementsByClassName("AFC2");
+	var afcRound2Array = document.getElementsByClassName("AFC2");
 
-  var afcStorageArray = [];
+	var afcStorageArray = [];
 
-  console.log("AFC Round 1 - " + afcRound1Array.length)
-  console.log("NFC Round 1 - " + nfcRound1Array .length)
+	console.log("AFC Round 1 - " + afcRound1Array.length)
+	console.log("NFC Round 1 - " + nfcRound1Array .length)
 
-  // AFC Round 1
-  for(var i=0; i< afcRound1Array.length; i++){
+	// AFC Round 1
+	for(var i=0; i< afcRound1Array.length; i++){
 
-    // Get the seed for the current html element
-    seed = parseInt(afcRound1Array[i].getAttribute("seed"))
+		// Get the seed for the current html element
+		seed = parseInt(afcRound1Array[i].getAttribute("seed"))
 
-    // Get the right team index
-    teamObject = binarySearch(teams, result.afcRound1[seed-1])
-    afcStorageArray[seed] = teamObject 
-    firstRoundLogic(teamObject, afcRound1Array)
-  }
+		// Get the right team index
+		teamObject = binarySearch(teams, result.afcRound1[seed-1])
+		afcStorageArray[seed] = teamObject 
+		teamStyleLogic(teamObject, afcRound1Array[i])
+	}
 
-  // NFC Round 1
-  for(var i=0; i< nfcRound1Array.length; i++){
+	// NFC Round 1
+	for(var i=0; i< nfcRound1Array.length; i++){
 
-    // Get the seed for the current html element
-    seed = parseInt(nfcRound1Array[i].getAttribute("seed"))
-    
-    // Get the right team index
-        teamObject = binarySearch(teams, result.nfcRound1[seed-1])
-    firstRoundLogic(teamObject , nfcRound1Array)
-  }
+		// Get the seed for the current html element
+		seed = parseInt(nfcRound1Array[i].getAttribute("seed"))
 
+		// Get the right team index
+		teamObject = binarySearch(teams, result.nfcRound1[seed-1])
+		teamStyleLogic(teamObject , nfcRound1Array[i])
+	}
 
-  console.log("size: " + afcStorageArray.length)
-  //Loop through storage array
-  for(var i=1; i< afcStorageArray.length ; i++){
-    if(data !== null && data !== '') {
-      console.log(i+ ": " + afcStorageArray[i].city)
-    } else {
-      console.log(i + "i: " + i)
-    }
+	// AFC Round 2
+	for(var i=0; i< afcRound2Array.length; i++){
+		seed = parseInt(afcRound2Array[i].getAttribute("seed"))
+		teamStyleLogic(afcStorageArray[seed],afcRound2Array[i])
+	}
 
-  }
-
-
-  // AFC Round 2
-    for(var i=0; i< afcRound2Array.length; i++){
-      secondRoundLogic(afcStorageArray[i],afcRound2Array)
-    }
-
-
-
-
-  // Grab json files
-  function parseJson(jsonObject){
-    try {
-      return jsonObject
-    } catch (e) {
-      console.error("Failed to parse JSON: " + e )
-      return null
-    }
-  }
-
-  // grab the the team info that matches the results value. 
-  // credit - wOxxOm (https://codereview.stackexchange.com/questions/144821/binary-search-on-string-in-alphabetical-order)
-  function binarySearch(haystack, needle) {
-
-    var a = 0;
-    var b = haystack.length - 1;
-    if (needle < haystack[0].city || needle > haystack[b].city) {
-      return {};
-    }
-    while (a < b - 1) {
-      var c = (a + b) / 2 |0;
-      if (needle < haystack[c].city) {
-        b = c;
-      } else {
-        a = c;
-      }
-    }
-    //TODO check for this edge case.  multiple teams play in the same city edge case
-    if(haystack[a].city == 'Los Angeles' || haystack[a].city == 'New York'){
-      return haystack[a]
-    } else if (haystack[a].city === needle){
-      return haystack[a]
-    } else if (haystack[a+1].city === needle) {
-      return haystack[a+1]
-    } else {
-      console.error("Something went wrong in binary search when searching for - " + needle)
-      return {}
-    }
-  }
-
-  // Logic used for getting populating first round teams
-  function firstRoundLogic(teamObject, divisionArray){
-
-    // Change Box to match teams information
-    divisionArray[i].style.background = teamObject.cPrim;
-    divisionArray[i].innerText = teamObject.city + " " +  teamObject.name;
-
-    // Line below name with secondary color
-    const firstSeedLine = document.createElement("div"); 
-    firstSeedLine.setAttribute("id", "line")
-    divisionArray[i].appendChild(firstSeedLine); 
-
-    firstSeedLine.style.background = teamObject.cSecn;
-  }
-
-  // Logic used for getting populating second round teams
-  function secondRoundLogic(teamObject, divisionArray){
-
-    // Change Box to match teams information
-    divisionArray[i].style.background = teamObject.cPrim;
-    divisionArray[i].innerText = teamObject.city + " " +  teamObject.name;
-
-    // Line below name with secondary color
-    const firstSeedLine = document.createElement("div"); 
-    firstSeedLine.setAttribute("id", "line")
-    divisionArray[i].appendChild(firstSeedLine); 
-
-    firstSeedLine.style.background = teamObject.cSecn;
-  }
 
 });
 
+
+// Grab json files
+function parseJson(jsonObject){
+	try {
+		return jsonObject
+	} catch (e) {
+		console.error("Failed to parse JSON: " + e )
+		return null
+	}
+}
+
+// grab the the team info that matches the results value. 
+// credit - wOxxOm (https://codereview.stackexchange.com/questions/144821/binary-search-on-string-in-alphabetical-order)
+function binarySearch(haystack, needle) {
+
+	var a = 0;
+	var b = haystack.length - 1;
+	if (needle < haystack[0].city || needle > haystack[b].city) {
+		return {};
+	}
+	while (a < b - 1) {
+		var c = (a + b) / 2 |0;
+		if (needle < haystack[c].city) {
+			b = c;
+		} else {
+			a = c;
+		}
+	}
+	//TODO check for this edge case.  multiple teams play in the same city edge case
+	if(haystack[a].city == 'Los Angeles' || haystack[a].city == 'New York'){
+		return haystack[a]
+	} else if (haystack[a].city === needle){
+		return haystack[a]
+	} else if (haystack[a+1].city === needle) {
+		return haystack[a+1]
+	} else {
+		console.error("Something went wrong in binary search when searching for - " + needle)
+		return {}
+	}
+}
+
+// Logic used for getting populating team buttons
+function teamStyleLogic(teamObject, divisionArrayElement){
+
+	// Change Box to match teams information
+	divisionArrayElement.style.background = teamObject.cPrim;
+	divisionArrayElement.innerText = teamObject.city + " " +  teamObject.name;
+
+	// Line below name with secondary color
+	const firstSeedLine = document.createElement("div"); 
+	firstSeedLine.setAttribute("id", "line")
+	divisionArrayElement.appendChild(firstSeedLine); 
+
+	firstSeedLine.style.background = teamObject.cSecn;
+}
 
