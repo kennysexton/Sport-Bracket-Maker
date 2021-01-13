3d4d3a397cdb4b88b980f60b3a46aa09e4b6d7ec
kennysexton <kennysexton4@gmail.com>
2021-01-13 16:57:31 -0500

    working view of user submitted picks

--- js/displayUsers.js
+++ js/displayUsers.js
@@ -4,63 +4,39 @@ var successCounter = 0;
 var tabLength = 0;
 
 
-
 document.addEventListener("DOMContentLoaded", function(){
 
 	// Grab users JSON data
 	var userSubmissions = parseJson(userPicks);
 
-	var afcRound1Array = document.getElementsByClassName("AFC");
-	var nfcRound1Array = document.getElementsByClassName("NFC");
-
-
-	// Grab team JSON datas
-	var teams = parseJson(data)
-	var result = parseJson(results)
 	tabLength = Object.keys(userSubmissions).length
-	console.log("tab length:" + tabLength)
+	console.log("number of user tabs:" + tabLength)
 
 	for (i in userSubmissions) {
-		console.log(i);
-		console.log(userSubmissions[i]);
+		//		console.log(i);
+		//		console.log(userSubmissions[i]);
 
 		// Create a tab per submission
-		$("#myTab").append("<li class='nav-item user'><a class='nav-link' id='"+i+"-tab' data-toggle='tab' href='#"+i+"' role='tab' aria-controls='"+i+"' aria-selected='false'>"+i+"</a></li>")
+		$("#myTab").append("<li class='nav-item user'><a class='nav-link' id='"+i+"-tab' data-toggle='tab' href='#"+i+"' role='tab' aria-controls='"+i+"' aria-selected='false'>"+cleanInput(i)+"</a></li>")
 
 		$("#myTabContent").append("<div class='tab-pane fade' id='"+i+"' role='tabpanel' aria-labelledby='"+i+"-tab'>"+userSubmissions[i]+"<div id='bracket-viewonly-replace-"+i+"' class='text-center'>TODO - spinner on load</div></div>")
 
 		// Populate dropdowns with the previously selected result
 		var selector = '#bracket-viewonly-replace-' +i;
-		console.log("Selector: " + selector)
+		//		console.log("Selector: " + selector)
 		selectorArray[j] = selector
 
 		$(selector).load('htmlSegments/bracket.html', function (response, status) {
 			//get selector
-			//			var user = selector
 			if( status == 'success'){
-				console.log("load success")
 				successCounter++;
 			} else {
 				console.error("failed to load a bracket in one of the tabs")
 			}
-			
+
 			// Run on last success
-			buildHandler(j)
-			//					console.log("appear twice?" + i)
-			//		
-			//					// Round 1 (qualified teams)
-			//					var afcStorageArray = firstRoundPopulate(afcRound1Array,teams, result.afcRound1)
-			//					var nfcStorageArray = firstRoundPopulate(nfcRound1Array,teams, result.nfcRound1)
-			//		
-			//					var choicesSelect = selector +" button[round]"
-			//					var choices=$(choicesSelect)
-			//		
-			//					console.log(choices.length)
-			//		
-			//					choices.each(function( index ) {
-			//						console.log("index: " + userSubmissions[i].charAt(index))
-			//						$( this ).attr('seed', userSubmissions[i].charAt(index))
-			//					});
+			buildHandler(userSubmissions)
+
 		});
 		j++
 	}
@@ -68,13 +44,71 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 //
-function buildHandler(j) {
-	console.log(successCounter)
-	console.log(tabLength)
+function buildHandler(userSubmissions) {
+
+	// Only run after all load events have passed
 	if(successCounter == tabLength){
-		console.log("start population")
+		for(var i=0; i< selectorArray.length; i++){
+
+			var afcRound1Array = document.getElementsByClassName("AFC");
+			var nfcRound1Array = document.getElementsByClassName("NFC");
+
+			// Grab team JSON datas
+			var teams = parseJson(data)
+			var result = parseJson(results)
+
+			console.log("saved in list! " + selectorArray[i]);
+			objIndex = getIdFromSelector(selectorArray[i])
+
+
+			// Round 1 (qualified teams)
+			var afcStorageArray = firstRoundPopulate(afcRound1Array,teams, result.afcRound1)
+			var nfcStorageArray = firstRoundPopulate(nfcRound1Array,teams, result.nfcRound1)
+
+			var choicesSelect = selectorArray[i] +" button[round]"
+			var choices=$(choicesSelect)
+
+			console.log(choices.length)
+
+			//TODO, should this have a catch?
+			var winnerDivision = userSubmissions[objIndex].charAt(7) 
+
+			choices.each(function(index) {
+
+				var currentSeed = userSubmissions[objIndex].charAt(index) 
+				$(this).attr('seed', currentSeed)
+
+				// Remove some classes & attributes
+				$(this).removeClass('dropdown-toggle')
+				$(this).removeClass('btn')
+				$(this).removeAttr('aria-haspopup')
+				$(this).removeAttr('data-toggle')
+
+				// AFC
+				if(index == 3){
+					console.log(winnerDivision)
+					console.log("superbowl!?")
+					console.log(currentSeed)
+					if(winnerDivision == 'A'){
+						teamStyleLogic(afcStorageArray[currentSeed], $(this).get(0))
+					} else {
+						teamStyleLogic(nfcStorageArray[currentSeed], $(this).get(0))
+					}
+
+				} else if (index == 0 || index == 2 || index == 5){ //AFC
+					teamStyleLogic(afcStorageArray[currentSeed], $(this).get(0))
+				} else {
+					teamStyleLogic(nfcStorageArray[currentSeed], $(this).get(0))
+				}
+
+			});
+		}
 	}
-//	for(var i=0; i< selectorArray.length; i++){
-//		console.log("saved in list!" + selectorArray[i]);
-//	}
 }
+
+
+function getIdFromSelector(selectorString){
+	return selectorString.split('-').pop()
+}
+
+
