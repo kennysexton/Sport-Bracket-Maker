f7b4f892d6926caebca28e5450b5cb2858ab2c0d
kennysexton <kennysexton4@gmail.com>
2021-02-03 12:03:09 -0500

    adding a binary serach support for duplicate city teams

--- js/utils.js
+++ js/utils.js
@@ -14,9 +14,14 @@ function binarySearch(haystack, needle) {
       a = c;
     }
   }
-  //TODO check for this edge case.  multiple teams play in the same city edge case
-  if (haystack[a].city == 'Los Angeles' || haystack[a].city == 'New York') {
-    return haystack[a]
+  //Multiple teams play in the same city edge case. Use format (New York*Giants)
+  if (needle.includes('*')) {
+    teamName = needle.split('*')[1] // City name will be after star. 
+    if(teamName === haystack[a].name){
+      return haystack[a]
+    } else { // I don't think any city in a sport has more that 2 professional team, so use second
+      return haystack[a-1]
+    }
   } else if (haystack[a].city === needle) {
     return haystack[a]
   } else if (haystack[a + 1].city === needle) {
