6aa1a04c4a76623c83fa1c1778c6813fcaada26d
kennysexton <kennysexton4@gmail.com>
2021-05-27 21:55:04 -0400

    comments

--- js/bracketManager.js
+++ js/bracketManager.js
@@ -322,9 +322,9 @@ $("#submissionForm").submit(function (e) {
   $.ajax({
     type: "POST",
     url: url,
-    crossDomain: true,  //TODO remove if useless
+    crossDomain: true,  
     data: form.serialize(), // serializes the form's elements.
-    headers: { //TODO remove if useless
+    headers: { 
       "accept": "application/json",
       "Access-Control-Allow-Origin": "*"
     },
